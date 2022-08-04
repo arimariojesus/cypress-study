30c184a4b1bfe8a825e11fa521671d604908f5c1
arimariojesus <arimario.jesus@hotmail.com>
2022-08-03 23:50:22 -0300

    test: ensure remove transactions correctly

--- cypress/integration/transactions.spec.ts
+++ cypress/integration/transactions.spec.ts
@@ -25,4 +25,42 @@ describe('ari.money', () => {
 
     cy.get('table > tbody > tr').should('have.length', 1)
   })
+  
+  it('Should remove transactions correctly', () => {
+    const incoming = { title: 'Salário', value: '1000' }
+    const outcoming = { title: 'Internet', value: '100' }
+    
+    cy.findByText('Nova transação').click()
+    cy.findByPlaceholderText('Título').type(incoming.title)
+    cy.findByPlaceholderText('Valor').type(incoming.value)
+    cy.findByText('Entrada').click()
+    cy.findByPlaceholderText('Categoria').type('Fixo')
+    cy.findByText('Cadastrar').click()
+    
+    cy.findByText('Nova transação').click()
+    cy.findByPlaceholderText('Título').type(outcoming.title)
+    cy.findByPlaceholderText('Valor').type(outcoming.value)
+    cy.findByText('Saída').click()
+    cy.findByPlaceholderText('Categoria').type('Fixo')
+    cy.findByText('Cadastrar').click()
+
+    cy.get('table > tbody > tr').should('have.length', 2)
+
+    cy.get('table > tbody')
+      .contains(incoming.title)
+      .parent()
+      .findByAltText('Remover transação')
+      .click()
+
+    cy.get('table > tbody > tr').contains(incoming.title).should('not.exist')
+    cy.get('table > tbody > tr').should('have.length', 1)
+
+    cy.get('table > tbody')
+      .contains(outcoming.title)
+      .parent()
+      .findByAltText('Remover transação')
+      .click()
+
+    cy.get('table > tbody > tr').should('have.length', 0)
+  })
 })
