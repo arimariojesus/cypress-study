d1c307230add3a7640df5d5b47afda4c66e58fb7
arimariojesus <arimario.jesus@hotmail.com>
2022-08-03 23:20:38 -0300

    test: ensure register a new outgoing transaction

--- cypress/integration/transactions.spec.ts
+++ cypress/integration/transactions.spec.ts
@@ -1,13 +1,25 @@
 describe('ari.money', () => {
-  it('Should register a new incoming transaction', () => {
+  beforeEach(() => {
     cy.visit('')
-
     cy.get('table > tbody > tr').should('have.length', 0)
-
+  })
+  
+  it('Should register a new incoming transaction', () => {
     cy.findByText('Nova transação').click()
     cy.findByPlaceholderText('Título').type('Salário')
     cy.findByPlaceholderText('Valor').type('1000')
     cy.findByText('Entrada').click()
+    cy.findByPlaceholderText('Categoria').type('Fixo')
+    cy.findByText('Cadastrar').click()
+
+    cy.get('table > tbody > tr').should('have.length', 1)
+  })
+  
+  it('Should register a new outgoing transaction', () => {
+    cy.findByText('Nova transação').click()
+    cy.findByPlaceholderText('Título').type('Salário')
+    cy.findByPlaceholderText('Valor').type('1000')
+    cy.findByText('Saída').click()
     cy.findByPlaceholderText('Categoria').type('Fixo')
     cy.findByText('Cadastrar').click()
 
