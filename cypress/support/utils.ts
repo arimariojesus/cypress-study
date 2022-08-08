854ebcfec959940e0e5e6fc61d4c5462f11d45b5
arimariojesus <arimario.jesus@hotmail.com>
2022-08-07 23:18:22 -0300

    feat: add formatDatTime util

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -34,3 +34,9 @@ export const prepareLocalStorage = (win: Window & typeof globalThis) => {
 
   return items
 }
+
+export function formatDateTime(date: Date | string) {
+  return new Intl.DateTimeFormat("pt-BR").format(
+    new Date(date)
+  );
+}
