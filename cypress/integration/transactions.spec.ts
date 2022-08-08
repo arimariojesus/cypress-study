b1e4c1bd5fa7a71e1cc97ca5ff523f16592f9610
arimariojesus <arimario.jesus@hotmail.com>
2022-08-07 21:30:06 -0300

    refactor: use helper to make transactions

--- cypress/integration/transactions.spec.ts
+++ cypress/integration/transactions.spec.ts
@@ -1,5 +1,32 @@
 import { format } from '../support/utils'
 
+interface TransactionReturn {
+  title: string
+  value: string
+}
+
+const makeIncomingTransaction = (): TransactionReturn => {
+  cy.findByText('Nova transação').click()
+  cy.findByPlaceholderText('Título').type('Mesada')
+  cy.findByPlaceholderText('Valor').type('500')
+  cy.findByText('Entrada').click()
+  cy.findByPlaceholderText('Categoria').type('Fixo')
+  cy.findByText('Cadastrar').click()
+
+  return { title: 'Mesada', value: '500' }
+};
+
+const makeExpenseTransaction = (): TransactionReturn => {
+  cy.findByText('Nova transação').click()
+  cy.findByPlaceholderText('Título').type('Pizza')
+  cy.findByPlaceholderText('Valor').type('100')
+  cy.findByText('Saída').click()
+  cy.findByPlaceholderText('Categoria').type('Comida')
+  cy.findByText('Cadastrar').click()
+
+  return { title: 'Pizza', value: '100' }
+};
+
 describe('ari.money', () => {
   beforeEach(() => {
     cy.visit('')
@@ -7,58 +34,34 @@ describe('ari.money', () => {
   })
   
   it('Should register a new incoming transaction', () => {
-    cy.findByText('Nova transação').click()
-    cy.findByPlaceholderText('Título').type('Salário')
-    cy.findByPlaceholderText('Valor').type('1000')
-    cy.findByText('Entrada').click()
-    cy.findByPlaceholderText('Categoria').type('Fixo')
-    cy.findByText('Cadastrar').click()
+    makeIncomingTransaction()
 
     cy.get('table > tbody > tr').should('have.length', 1)
   })
   
-  it('Should register a new outgoing transaction', () => {
-    cy.findByText('Nova transação').click()
-    cy.findByPlaceholderText('Título').type('Salário')
-    cy.findByPlaceholderText('Valor').type('1000')
-    cy.findByText('Saída').click()
-    cy.findByPlaceholderText('Categoria').type('Fixo')
-    cy.findByText('Cadastrar').click()
+  it('Should register a new expense transaction', () => {
+    makeExpenseTransaction()
 
     cy.get('table > tbody > tr').should('have.length', 1)
   })
   
   it('Should remove transactions correctly', () => {
-    const incoming = { title: 'Salário', value: '1000' }
-    const outcoming = { title: 'Internet', value: '100' }
-    
-    cy.findByText('Nova transação').click()
-    cy.findByPlaceholderText('Título').type(incoming.title)
-    cy.findByPlaceholderText('Valor').type(incoming.value)
-    cy.findByText('Entrada').click()
-    cy.findByPlaceholderText('Categoria').type('Fixo')
-    cy.findByText('Cadastrar').click()
-    
-    cy.findByText('Nova transação').click()
-    cy.findByPlaceholderText('Título').type(outcoming.title)
-    cy.findByPlaceholderText('Valor').type(outcoming.value)
-    cy.findByText('Saída').click()
-    cy.findByPlaceholderText('Categoria').type('Fixo')
-    cy.findByText('Cadastrar').click()
+    const income = makeIncomingTransaction()
+    const expense = makeExpenseTransaction()
 
     cy.get('table > tbody > tr').should('have.length', 2)
 
     cy.get('table > tbody')
-      .contains(incoming.title)
+      .contains(income.title)
       .parent()
       .findByAltText('Remover transação')
       .click()
 
-    cy.get('table > tbody > tr').contains(incoming.title).should('not.exist')
+    cy.get('table > tbody > tr').contains(income.title).should('not.exist')
     cy.get('table > tbody > tr').should('have.length', 1)
 
     cy.get('table > tbody')
-      .contains(outcoming.title)
+      .contains(expense.title)
       .parent()
       .findByAltText('Remover transação')
       .click()
@@ -66,21 +69,10 @@ describe('ari.money', () => {
     cy.get('table > tbody > tr').should('have.length', 0)
   })
   
-  it('Should have correct total balance', () => {
+  it('Should have correct balance', () => {
     
-    cy.findByText('Nova transação').click()
-    cy.findByPlaceholderText('Título').type('Mesada')
-    cy.findByPlaceholderText('Valor').type('500')
-    cy.findByText('Entrada').click()
-    cy.findByPlaceholderText('Categoria').type('Fixo')
-    cy.findByText('Cadastrar').click()
-    
-    cy.findByText('Nova transação').click()
-    cy.findByPlaceholderText('Título').type('Pizza')
-    cy.findByPlaceholderText('Valor').type('100')
-    cy.findByText('Saída').click()
-    cy.findByPlaceholderText('Categoria').type('Comida')
-    cy.findByText('Cadastrar').click()
+    makeIncomingTransaction()
+    makeExpenseTransaction()
 
     let incomes = 0
     let expenses = 0
